89da63d8d30fe4a77dc5285a6faf169b9fc76c57
Mr-Mhlongo <syaonmhlongo100@gmail.com>
2025-09-16 19:45:57 +0200

    updated express server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express , {Express} from "express";
 import bodyParser from "body-parser"
 import { authorsRouter } from "./routes/authors"
 import { logger } from "./middleware/logger"
+import { booksRouter } from "./routes/books"
 
 const app: Express =express()
 const PORT = process.env.PORT ||3000
@@ -10,6 +11,7 @@ app.use(bodyParser.json())
 app.use(logger)
 
 app.use("/authors", authorsRouter)
+app.use("/books", booksRouter)
 
 
 app.listen(PORT, () => {
