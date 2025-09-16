5b2c055917ad8009b8ef8295f8da6633be48251b
Mr-Mhlongo <syaonmhlongo100@gmail.com>
2025-09-16 19:19:51 +0200

    express server updated

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
 import express , {Express} from "express";
+import bodyParser from "body-parser"
+import { authorsRouter } from "./routes/authors"
+import { logger } from "./middleware/logger"
 
 const app: Express =express()
 const PORT = process.env.PORT ||3000
+
+app.use(bodyParser.json())
+app.use(logger)
+
+app.use("/authors", authorsRouter)
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
